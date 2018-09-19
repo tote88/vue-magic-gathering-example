c6294ec5f071c0c4fba20d2a828f3b0b011c0fad
jordi.casanovas <jordi.casanovas@fib.upc.edu>
2018-09-19 16:39:40 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div>
+      <button @click="getDeck">new Deck</button>
+    </div>
+    <card v-for="item in cards" :key="item.id" :name="item.name" :image="item.imageUrl"></card>
+  </div>
+</template>
+
+<script>
+import Card from './components/card'
+export default {
+  name: 'App',
+  components: {
+    Card
+  },
+  data () {
+    return { 
+      cards: []
+    }
+  },
+  methods: {
+    getDeck: function() {
+      fetch("https://api.magicthegathering.io/v1/cards?pageSize=7&random=true")
+      .then((data) => data.json())
+      .then(data => this.cards = data.cards)
+    }
+  }
+}
+</script>
---
+++ src/components/card.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="card">
+        <img v-if="image !== undefined" :src="image">
+        <div v-else>Not Available</div>
+        <h4> {{name}} </h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'card',
+    props: ["name", "image"],
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+.h4 {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
+}
+
+img.card {
+    width: 200px;
+}
+
+.card {
+    display: inline-block;
+    margin: 4px;
+}
+</style>
+
+
